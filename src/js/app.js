5487e1a4f4a3296506f9df2318245c81d3455647
JULIO-EDUARDO-LARA <julioelara@gmail.com>
2023-10-08 07:28:59 -0500

    231008 cambios varios

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,34 @@
-//INICIO SLIDER
+/*
+0. LOGIN
+1. SLIDER
+2. EFECTO PARA TEXTO CIRCULAR EN BOTONES*/
+
+
+window.onload = function () {
+
+
+
+//0. LOGIN -----------------------------------------------------
+const loginShow = document.querySelector('.btnHeader');
+const sectionLogin = document.querySelector('.login');
+
+const cerrarLogin = document.querySelector('.login__cierre');
+
+loginShow.addEventListener('click', (e)=>{
+    e.preventDefault();
+    sectionLogin.classList.add('login--show');
+});
+
+if(cerrarLogin != null){
+    cerrarLogin.addEventListener('click', (e)=>{
+        e.preventDefault();
+        sectionLogin.classList.remove('login--show');
+    });
+}
+
+
+
+//1. SLIDER-----------------------------------------------------
 const btn_left = document.querySelector('.btn-left');
 const btn_right = document.querySelector('.btn-right');
 const slider = document.querySelector('#slider');
@@ -9,12 +39,17 @@ let cont = 0;
 let cantImg = slider_section.length;
 let anchoImg = 100 / cantImg;
 
-btn_left.addEventListener("click", e => deslizarIzquierda());
-btn_right.addEventListener("click", e => deslizarDerecha());
+if( btn_left != null && btn_left != null ){
+    btn_left.addEventListener("click", e => deslizarIzquierda());
+    btn_right.addEventListener("click", e => deslizarDerecha());
 
-setInterval(() => {
-    deslizarDerecha();
-},2500);
+    setInterval(() => {
+        deslizarDerecha();
+    },2500);
+}
+
+
+
 
 function deslizarDerecha () {
 
@@ -45,5 +80,15 @@ function deslizarIzquierda () {
     slider.style.transition = `all ease .6s`;
 }
 
+//2. EFECTO PARA TEXTO CIRCULAR EN BOTONES-----------------------------------------------------
+    //colocar cada letra en una etiqueta <span>
+    circle = document.querySelector(".texto-btn-icon");
+   
+    circleArray = circle.textContent.split('');
+    
+    circle.textContent = '';
 
-//FIN SLIDER
+    for(var i = 0; i < circleArray.length; i++){
+        circle.innerHTML += '<span class="span">'+circleArray[i]+'</span>';
+    }
+}
