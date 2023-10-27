870c0d2f389d86edee62fa9921e9ed0b9b1422ec
JULIO-EDUARDO-LARA <julioelara@gmail.com>
2023-10-27 15:19:43 -0500

    23.10.27 15:19

--- src/js/app.js
+++ src/js/app.js
@@ -77,24 +77,11 @@ window.onload = function  () {
         slider.style.transition = `all ease .6s`;
     }
 
-    //2. EFECTO PARA TEXTO CIRCULAR EN BOTONES-----------------------------------------------------
-        //colocar cada letra en una etiqueta <span>
 
-    circle = document.querySelector(".texto-btn-icon");
-    // console.log(circle)
+    //2. FUNCION GENERADOR DE BOTON CIRCULAR---------------------
 
-    if(circle){
-        
-        circleArray = circle.textContent.split('');
-        if(circleArray){
-            circle.textContent = '';
 
-            for(var i = 0; i < circleArray.length; i++){
-                circle.innerHTML += '<span class="span">'+circleArray[i]+'</span>';
-            }   
-        }
-     
-    }
+
 
     //3. EFECTOS PARA MENU DE LA NAVEGACION USUARIOS-----------------------------------------------------
 
@@ -250,70 +237,159 @@ window.onload = function  () {
         })        
     }
 
-    //5. EFECTOS PARA BOTONES DE FORMULARIO (CREAR EVENTO)------------------------------------------
+    //5. EFECTOS DESPLEGABLES PARA BOTONES DE FORMULARIO (CREAR EVENTO)------------------------------------------
     const btns_slide = document.querySelectorAll('.formBtn__eventCreate--deslizar');
     const fieldsets_slide = document.querySelectorAll('.form-grupo');
 
-    btns_slide.forEach((btn_slide, index1) => {
-        btn_slide.addEventListener('click', (e) => {
-            let btn_slide_clicked = e.target;
-            let selected_fields = btn_slide_clicked.nextElementSibling;
-    
-            fieldsets_slide.forEach((fieldset_slide, index2) => {
-                if(index2 != index1){
-                    if(fieldsets_slide[index2].classList.contains("form-grupo--desplegar")){
-                        console.log(fieldsets_slide[index2].classList.contains("form-grupo--desplegar"));
-                        fieldsets_slide[index2].classList.remove("form-grupo--desplegar");
-                        fieldsets_slide[index2].style.height = `0px`;
+    if(btns_slide != null){
+        btns_slide.forEach((btn_slide, index1) => {
+            btn_slide.addEventListener('click', (e) => {
+                let btn_slide_clicked = e.target;
+                let selected_fields = btn_slide_clicked.nextElementSibling;
+                let flechaClick = btn_slide_clicked.querySelector(".formulario__iconContainer--arrow");
+                // console.log(flechaClick);
+        
+                fieldsets_slide.forEach((fieldset_slide, index2) => {
+                    if(index2 != index1){
+                        if(fieldsets_slide[index2].classList.contains("form-grupo--desplegar")){
+                            fieldsets_slide[index2].classList.remove("form-grupo--desplegar");
+                            let flecha = btns_slide[index2].querySelector(".formulario__iconContainer--arrow");
+                            if(flecha.classList.contains("formulario__iconContainer--arrowRotate")){
+                                flecha.classList.remove("formulario__iconContainer--arrowRotate");
+                            }
+                            
+
+                            fieldsets_slide[index2].style.height = `0px`;
+                        }
                     }
+                })
+                selected_fields.classList.toggle("form-grupo--desplegar");
+                flechaClick.classList.toggle("formulario__iconContainer--arrowRotate");
+                let altura = 0;
+
+                let catchHeight_fielset = selected_fields.clientHeight;
+                // console.log("altura actual "+ catchHeight_fielset);
+
+                let catchMinHeight_fielset = selected_fields.scrollHeight;
+
+                if(catchHeight_fielset == "0"){
+                    altura = catchMinHeight_fielset;
+                }else{
+                    selected_fields.style.height = `0px`;
                 }
+                selected_fields.style.height = `${altura}px`;
+                return;
+
             })
-            selected_fields.classList.toggle("form-grupo--desplegar");
-            let altura = 0;
+        })        
+    }
 
-            let catchHeight_fielset = selected_fields.clientHeight;
-            let catchMinHeight_fielset = selected_fields.scrollHeight;
 
-            if(catchHeight_fielset == "0"){
-                altura = catchMinHeight_fielset;
-            }else{
-                selected_fields.style.height = `0px`;
-            }
-            selected_fields.style.height = `${altura}px`;
-            return;
+    //FUNCION SCRIPT PREVIZUALIZAR FOTO------------------------------------------
 
-        })
-    })
-    //5.1 SCRIPT PREVIZUALIZAR FOTO, FORMULARIO (CREAR EVENTO)------------------------------------------
-    const fileImg = document.querySelector(".set__data--upLoadImg");
-    const imgEventDefault = '../build/img/default/imagen-service-product.png';
-    const imagenCrearEvento = document.getElementById('01campo__img');
-    const campo01__label = document.getElementById('01campo__label');
-    fileImg.addEventListener('change', e =>{
-        let seCargoFoto = e.target.files[0];
-        if(seCargoFoto){
-            
-            const reader = new FileReader();
-            reader.onload = function(e) {
-                imagenCrearEvento.src = e.target.result;
-            }
-            campo01__label.style.display = "inline-block";
-            reader.readAsDataURL(seCargoFoto);
-            
-        }else{
-            imagenCrearEvento.src = imgEventDefault;
-            campo01__label.style.display = "none";
-
+    function previsualizarImg (inputUpload, imgDefault, campoImg, laLabel){
+        if(inputUpload != null){
+            inputUpload.addEventListener('change', e =>{
+                let seCargoFoto = e.target.files[0];
+                if(seCargoFoto){
+                    const reader = new FileReader();
+                    reader.onload = function(e) {
+                        campoImg.src = e.target.result;
+                    }
+                    laLabel.style.display = "inline-block";
+                    laLabel.style.color = "#FF0000";
+                    laLabel.innerText = "reemplazar imágen";
+                    reader.readAsDataURL(seCargoFoto);
+                }else{
+                    campoImg.src = imgDefault;
+                }
+            })        
         }
-    })
+    }
+    //5.1 SCRIPT PREVIZUALIZAR FOTO, PAGINA (CREAR EVENTO)-----------------------
+    const inputUploadImg0101 = document.getElementById("inputUploadImg0101");
+    const imgEventDefault = '../build/img/default/imagen-service-product.png';
+    const campo__img0101 = document.getElementById('campo__img0101');
+    const campo__label0101 = document.getElementById('campo__label0101');
+
+    previsualizarImg(inputUploadImg0101, imgEventDefault, campo__img0101, campo__label0101);
+
+    //5.2 SCRIPT PREVIZUALIZAR FOTO, PAGINA (CREAR SERVICIO)------------------------------------------
+    const inputUploadImg0102 = document.getElementById("inputUploadImg0102");
+    const imgServiceDefault = '../build/img/default/imagen-service-product.png';
+    const campo__img0102 = document.getElementById('campo__img0102');
+    const campo__label0102 = document.getElementById('campo__label0102');
+
+    previsualizarImg(inputUploadImg0102, imgServiceDefault, campo__img0102, campo__label0102);
 
 
+    //5.3 SCRIPT PREVIZUALIZAR FOTO, PAGINA EDITAR PERFIL (SECCION PERSONAL)--------------------------
+    const inputUploadImg0201 = document.getElementById("inputUploadImg0201");
+    const imgPersonalDefault = "../build/img/default/usuario.png";
+    const campo__img0201 = document.getElementById('campo__img0201');
+    const campo__label0201 = document.getElementById('campo__label0201');
+
+    previsualizarImg(inputUploadImg0201, imgPersonalDefault, campo__img0201, campo__label0201);
 
 
+    //5.4 SCRIPT PREVIZUALIZAR FOTO, PAGINA EDITAR PERFIL (PROMOTOR)------------------------------------------
+    const inputUploadImg0202 = document.getElementById("inputUploadImg0202");
+    const imgPromotorDefault = "../build/img/default/foto_bg_empresa.png";
+    const campo__img0202 = document.getElementById('campo__img0202');
+    const campo__label0202 = document.getElementById('campo__label0202');
+
+    previsualizarImg(inputUploadImg0202, imgPromotorDefault, campo__img0202, campo__label0202);
+
+    //6. EFECTOS DESPLEGABLES PARA BOTONES DE FORMULARIO (CREAR EVENTO)------------------------------------------
+
+    const btns_despliegue = document.querySelectorAll(".estadoEvento--datos .estadoEvento__h2Container");
+    const boxs_despliegue = document.querySelectorAll(".estadoEvento--datos .tablaEstadoContainer");
+   
+    // console.log(box_despliegue);
+    if(btns_despliegue != null){
+        btns_despliegue.forEach( (btn_despliegue, index1 )=> {
+            btn_despliegue.addEventListener('click', (e) => {
+                let btn_slide_clicked = e.target;
+                let selected_box = btn_slide_clicked.nextElementSibling;
+                let flechaClick = btn_slide_clicked.querySelector(".iconContainer--arrow");
 
 
+          
+
+                boxs_despliegue.forEach((fieldset_slide, index2) => {
+                    if(index2 != index1){
+
+                        if(boxs_despliegue[index2].classList.contains("tablaEstadoContainer--desplegar")){
+                            boxs_despliegue[index2].classList.remove("tablaEstadoContainer--desplegar");
+                            let flecha = btns_despliegue[index2].querySelector(".iconContainer--arrow");
+                            if(flecha.classList.contains("iconContainer--arrowRotate")){
+                                flecha.classList.remove("iconContainer--arrowRotate");
+                            }
+                            boxs_despliegue[index2].style.height = `0px`;
+                        }
+                    }
+                })
+                selected_box.classList.toggle("tablaEstadoContainer--desplegar");
+                flechaClick.classList.toggle("iconContainer--arrowRotate");
+                let altura = 0;
+                
+                let catchHeight_fielset = selected_box.clientHeight;
+                // console.log("altura actual "+ catchHeight_fielset);
+                let catchMinHeight_fielset = selected_box.scrollHeight;
+                console.log("altura minima "+ catchMinHeight_fielset);
 
 
+                if(catchHeight_fielset == "0"){
+                    altura = catchMinHeight_fielset;
+                }else{
+                    selected_box.style.height = `0px`;
+                }
+                selected_box.style.height = `${altura}px`;
+                return;
+
+            } )
+        })        
+    }
 
 
 }
